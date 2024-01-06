209f2515775507ebd7735afe4f6c0b91f457a542
Yosaishu <codehego@gmail.com>
2024-01-06 23:53:50 +0300

    C0. Life the game first commit

---
+++ src/enumerate/Box.java
@@ -0,0 +1,37 @@
+package enumerate;
+
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Box extends JPanel {
+
+    Cell cell;
+
+    public Box(int x, int y) {
+        super();
+        cell = new Cell();
+        setBounds(x * Config.SIZE, y * Config.SIZE, Config.SIZE, Config.SIZE);
+        setBackground(Config.getColor(Status.NONE));
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                cell.turn();
+            }
+        });
+    }
+
+    public void setColor() {
+        setBackground(Config.getColor(cell.status));
+    }
+
+    public void step1() {
+        cell.step1();
+        setColor();
+    }
+
+    public void step2() {
+        cell.step2();
+        setColor();
+    }
+}
